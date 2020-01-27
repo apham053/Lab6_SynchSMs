7cb1d7defa5ad2606ddb7956c0cf8950219f8820
Anthony Pham <apham053@wch136-19.cs.ucr.edu>
2020-01-27 15:37:37 -0800

    ex3 start

--- SynchSMs/source/main.c
+++ SynchSMs/source/main.c
@@ -1,7 +1,7 @@
 /*	Author: apham053
  *  Partner(s) Name: Steven Rodriguez
  *	Lab Section:
- *	Assignment: Lab #6  Exercise #2
+ *	Assignment: Lab #6  Exercise #3
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -48,63 +48,96 @@ void TimerSet(unsigned long M) {
 	_avr_timer_cntcurr = _avr_timer_M;
 }
  
+unsigned char C = 0x07;        
+#define A0 (PINA & 0x01)  
+#define A1 (PINA & 0x02)  
 
-    enum States { Start, blink1, blink2, blink3, wait } State;
-    unsigned char B = 0x00; 
-    #define A0 (PINA & 0x01)
-    
-    void tick() {
-	switch(State) {
-	    case Start:
-		B = 0x00;
-		State = blink1;
-		break;
-	    case blink1:
-		B = 0x01; 
-		State = A0 ? wait : blink2;
-		break;
-	    case blink2:
-		B = 0x02;
-		State = A0 ? wait : blink3;
-		break;
-	    case blink3: 
-		B = 0x04;
-		State = A0 ? wait : blink1;
-		break;
-	    case wait:
-		State = A0 ? blink1 : wait; 
-		break;
-	    default: 
+enum States { Start, Wait, Add, Sub, Reset, Release} State;
+
+void tick() {
+    switch(State) {
+        case Start:
+	    C = 0x07;       
+	    State = Wait; 	
+	    break;
+	case Wait: 
+		if (A0 && !A1) {
+			State = Add;
+		}
+		else if (!A0 && A1) {
+			State = Sub;
+		}
+		else if (A0 && A1) {
+			State = Reset;
+		}
+		else {
+			State = Wait;
+		}
+	    break;
+	case Add:   
+		State = Release;
+	    break;
+	case Sub:
+		State = Release;
+            break;
+	case Reset:
+		State = Release;
+	    break;
+	case Release:
+		if (!A0 && !A1) {
+			State = Wait;
+		}
+		else if (A0 && A1) {
+			State = Reset;
+		}
+		else { 
+			State = Release;
+		}
+	        break;
+    	default:
 		State = Start;
 		break;
-   	}
-
-	switch(State) {
-	    case Start:
-		break;
-	    case blink1:
-                break;
-	    case blink2:
-                break;
-	    case blink3:
-                break;
-	    case wait:
-		break;
-	    default:
-		break;
-	}
-	
-	PORTB = B;
     }
+    
+    switch(State) {
+	case Start:
+	    break;
+	case Wait:
+	    break;
+	case Add:
+	    if (C < 0x09) {
+                C = C + 1;
+            }
+	    else {
+                C = 0x09;
+            }
+            break;
+	case Sub:
+	    if (C > 0x00) {
+		C = C - 1;
+	    }
+	    else {
+		C = 0x00;
+	    }
+            break;
+	case Reset:
+	    C = 0x00;
+            break;
+    case Release:
+		break;
+	default:
+	    break;	    
+    }    
+}   
 
 int main (void) {
     DDRA = 0x00;
-    DDRB = 0xFF;
+    DDRC = 0xFF;
     PORTA = 0xFF;
-    PORTB = 0x00;
-    B = 0x00; 
+    PORTC = 0x00;
+    C = 0x00; 
     State = Start;
-    TimerSet(300); 
+    TimerSet(100); 
     TimerOn();	    
     
     while(1) {
