52d3b47841700eee3b7a63e19ded5eeb7f6fbac4
Anthony Pham <apham053@wch136-19.cs.ucr.edu>
2020-02-10 11:07:59 -0800

    yes

--- SynchSMs/source/main.c
+++ SynchSMs/source/main.c
@@ -9,11 +9,13 @@
  */
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <stdio.h> 
+#include <stdlib.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
-   
-volatile unsigned char TimerFlag = 0;  //
+
+volatile unsigned char TimerFlag = 0;  
 
 unsigned long _avr_timer_M = 1; 
 unsigned long _avr_timer_cntcurr = 0; 
@@ -31,7 +33,7 @@ void TimerOff() {
   TCCR1B = 0x00;
 }
 
-void TimerISR() {        //
+void TimerISR() {        
   TimerFlag = 1;
 }  
 
@@ -47,110 +49,115 @@ void TimerSet(unsigned long M) {
 	_avr_timer_M = M;
 	_avr_timer_cntcurr = _avr_timer_M;
 }
- 
-unsigned char C = 0x07;        
-#define A0 (PINA & 0x01)  
-#define A1 (PINA & 0x02)  
 
-enum States { Start, Wait, Add, Sub, Reset, Release} State;
+void set_PWM(double frequency) {
+  static double current_frequency;
+  if (frequency != current_frequency) {
+    if (!frequency) { TCCR3B &= 0x08; }
+    else { TCCR3B |= 0x03; }
+    if (frequency < 0.954) { OCR3A = 0xFFFF; }
+    else if (frequency > 31250) { OCR3A = 0x0000; }
+    else { OCR3A = (short) (8000000 / (128 * frequency)) - 1; }
+    TCNT3 = 0;
+    current_frequency = frequency;
+  }
+}
+
+void PWM_on() {
+  TCCR3A = (1 << COM3A0);
+  TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+  set_PWM(0);
+}
+
+void PWM_off() {
+  TCCR3A = 0x00;
+  TCCR3B = 0x00;
+}
+
+#define A0 (~PINA & 0x01)
+unsigned char arrIndex = 0;
+unsigned char i = 8;
+double noteArr[8] = {261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25};
+enum States { start, wait, play, hold, stop} State;
 
 void tick() {
     switch(State) {
-        case Start:
-	    C = 0x07;       
-	    State = Wait; 	
+	case start:
+	    State = wait;
 	    break;
-	case Wait: 
-		if (A0 && !A1) {
-			State = Add;
-		}
-		else if (!A0 && A1) {
-			State = Sub;
-		}
-		else if (A0 && A1) {
-			State = Reset;
+	case wait:
+	    i = 8;
+	    if (A0) {
+	        State = play;    
+	    }
+	    else {
+		State = wait;
+	    }
+	    break;
+	case play:
+	    if (i > 0) {
+		i--;
+		State = play;
+	    }
+	    else {
+		if (A0) {
+		    State = hold;
 		}
 		else {
-			State = Wait;
+		    State = stop;
 		}
+	    }
 	    break;
-	case Add:   
-		State = Release;
+	case hold:
+	    if (!A0) {
+		State = wait;
+	    }
+	    else {
+	        State = hold;
+	    }
 	    break;
-	case Sub:
-		State = Release;
-            break;
-	case Reset:
-		State = Release;
+	case stop:
+	    State = wait;
 	    break;
-	case Release:
-		if (!A0 && !A1) {
-			State = Wait;
-		}
-		else if (A0 && A1) {
-			State = Reset;
-		}
-		else if (A0 && !A1) {
-			State = Add;
-		}
-		else if (!A0 && A1) {
-                        State = Sub;
-                }
-		else { 
-			State = Release;
-		}
-	        break;
-    	default:
-		State = Start;
-		break;
+	default:
+	    State = start;
+	    break;
     }
     
     switch(State) {
-	case Start:
+	case start:
+	    set_PWM(0);
 	    break;
-	case Wait:
-	    break;
-	case Add:
-	    if (C < 0x09) {
-                C = C + 1;
-            }
-	    else {
-                C = 0x09;
-            }
+	case wait:
             break;
-	case Sub:
-	    if (C > 0x00) {
-		C = C - 1;
-	    }
-	    else {
-		C = 0x00;
-	    }
+	case play:
+	    arrIndex = (rand() % 8);
+	    set_PWM(noteArr[arrIndex]);
             break;
-	case Reset:
-	    C = 0x00;
+	case hold:
             break;
-        case Release:
-		break;
+	case stop:
+            break;
 	default:
-	    break;	    
-    }    
-}   
+            break;
+    }
+}
 
-int main (void) {
+int main(void) {
     DDRA = 0x00;
-    DDRC = 0xFF;
+    DDRB = 0xFF;
     PORTA = 0xFF;
-    PORTC = 0x00;
-    C = 0x07; 
-    State = Start;
-    TimerSet(100); 
-    TimerOn();	    
-    
-    while(1) {
+    PORTB = 0x00;
+    State = start;
+    PWM_on();
+    TimerSet(50);
+    TimerOn();
+
+    while(1){
 	tick();
 	while(!TimerFlag) {}
-	    TimerFlag = 0;
-	    PORTC = C;	
-    }	   
+	TimerFlag = 0;
+    }
+ 
     return 1;
 }
